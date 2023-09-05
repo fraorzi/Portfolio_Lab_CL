208e0225aa69aefeb405ce8d290d711987ebc309
fraorzi <orzechowskifranek@gmail.com>
2023-09-05 18:02:44 +0200

    pierwsze pliki

---
+++ portfolio-lab-cl/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
+import './App.css';
+
+function App() {
+    return (
+        <Router>
+            <Switch>
+                <Route path="/" exact>
+                    <Home />
+                </Route>
+            </Switch>
+        </Router>
+    );
+}
+
+export default App;
---
+++ portfolio-lab-cl/src/components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import HomeHeader from './HomeHeader';
+import HomeThreeColumns from './HomeThreeColumns';
+import './Home.css';
+
+function Home() {
+    return (
+        <div>
+            <HomeHeader />
+            <HomeThreeColumns />
+        </div>
+    );
+}
+
+export default Home;
