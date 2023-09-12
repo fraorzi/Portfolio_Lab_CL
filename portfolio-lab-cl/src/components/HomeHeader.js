f87d9533ca19c1eb3adc423d369cbadd8f385006
fraorzi <orzechowskifranek@gmail.com>
2023-09-12 03:07:46 +0200

    added ThreeColumns and functions to them

--- portfolio-lab-cl/src/components/HomeHeader.js
+++ portfolio-lab-cl/src/components/HomeHeader.js
@@ -4,23 +4,29 @@ import { Link } from 'react-router-dom';
 function HomeHeader() {
     return (
         <div className="home-header">
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to="/login">Zaloguj się</Link>
-                        </li>
-                        <li>
-                            <Link to="/register">Zarejestruj się</Link>
-                        </li>
-                    </ul>
-                </nav>
-                <div className="header-buttons">
-                    <Link to="/" className="header-button">Start</Link>
-                    <Link to="/" className="header-button">O co chodzi?</Link>
-                    <Link to="/" className="header-button">O nas</Link>
-                    <Link to="/" className="header-button">Fundacja i organizacje</Link>
-                    <Link to="/" className="header-button">Kontakt</Link>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/login">Zaloguj się</Link>
+                    </li>
+                    <li>
+                        <Link to="/register">Zarejestruj się</Link>
+                    </li>
+                </ul>
+            </nav>
+            <div className="header-buttons">
+
+                <div className="main-buttons">
+                    <Link to="/login" className="main-button">Oddaj rzeczy</Link>
+                    <Link to="/login" className="main-button">Zorganizuj zbiórkę</Link>
                 </div>
+
+                <Link to="start-section" smooth={true} className="header-button">Start</Link>
+                <Link to="about-section" smooth={true} className="header-button">O co chodzi?</Link>
+                <Link to="about-us-section" smooth={true} className="header-button">O nas</Link>
+                <Link to="foundation-section" smooth={true} className="header-button">Fundacja i organizacje</Link>
+                <Link to="contact-section" smooth={true} className="header-button">Kontakt</Link>
+            </div>
         </div>
     );
 }
--- portfolio-lab-cl/src/components/HomeThreeColumns.js
+++ portfolio-lab-cl/src/components/HomeThreeColumns.js
@@ -3,7 +3,21 @@ import React from 'react';
 function HomeThreeColumns() {
     return (
         <div className="home-three-columns">
-
+            <div className="statistic-column">
+                <h1>10</h1>
+                <p>ODDANYCH WORKÓW</p>
+                <span>Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.</span>
+            </div>
+            <div className="statistic-column">
+                <h1>5</h1>
+                <p>WSPARTYCH ORGANIZACJI</p>
+                <span>Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.</span>
+            </div>
+            <div className="statistic-column">
+                <h1>7</h1>
+                <p>ZORGANIZOWANYCH ZBIÓREK</p>
+                <span>Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.</span>
+            </div>
         </div>
     );
 }
