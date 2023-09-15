7b2279ada4e17f4cf82fd171f549ad62728c9fff
fraorzi <orzechowskifranek@gmail.com>
2023-09-15 06:25:10 +0200

    Refactor pagination logic using Math.ceil and slice

---
+++ portfolio-lab-cl/src/components/Data/Data.js
@@ -0,0 +1,163 @@
+const categoriesData = {
+    fundations: {
+        title: 'Fundacjom',
+        description: 'W naszej bazie znajdziesz listę zweryfikowanych Fundacji, z którymi współpracujemy. Możesz sprawdzić czym się zajmują, komu pomagają i czego potrzebują.',
+        items: [
+            {
+                name: 'Fundacja “Dbam o Zdrowie”',
+                description: 'Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej.',
+                tags: 'ubrania, jedzenie, sprzęt AGD, meble, zabawki'
+            },
+            {
+                name: 'Fundacja “Dla dzieci”',
+                description: 'Cel i misja: Pomoc dzieciom z ubogich rodzin.',
+                tags: 'ubrania, meble, zabawki'
+            },
+            {
+                name: 'Fundacja “Bez domu1”',
+                description: 'Cel i misja: Pomoc dla osób bezdomnych.',
+                tags: 'jedzenie, ciepłe ubrania'
+            },
+            {
+                name: 'Fundacja “Bez domu2”',
+                description: 'Cel i misja: Pomoc dla osób bezdomnych.',
+                tags: 'jedzenie, ciepłe ubrania'
+            },
+            {
+                name: 'Fundacja “Bez domu3”',
+                description: 'Cel i misja: Pomoc dla osób bezdomnych.',
+                tags: 'jedzenie, ciepłe ubrania'
+            },
+            {
+                name: 'Fundacja “Bez domu4”',
+                description: 'Cel i misja: Pomoc dla osób bezdomnych.',
+                tags: 'jedzenie, ciepłe ubrania'
+            },
+            {
+                name: 'Fundacja “Bez domu5”',
+                description: 'Cel i misja: Pomoc dla osób bezdomnych.',
+                tags: 'jedzenie, ciepłe ubrania'
+            },
+            {
+                name: 'Fundacja “Bez domu6”',
+                description: 'Cel i misja: Pomoc dla osób bezdomnych.',
+                tags: 'jedzenie, ciepłe ubrania'
+            },
+            {
+                name: 'Fundacja “Bez domu7”',
+                description: 'Cel i misja: Pomoc dla osób bezdomnych.',
+                tags: 'jedzenie, ciepłe ubrania'
+            },
+            {
+                name: 'Fundacja “Bez domu8”',
+                description: 'Cel i misja: Pomoc dla osób bezdomnych.',
+                tags: 'jedzenie, ciepłe ubrania'
+            }
+
+
+
+        ]
+
+    },
+    organizations: {
+        title: 'Organizacjom pozarządowym',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.',
+        items: [
+            {
+                name: 'Organizacja “Lorem Ipsum 1”',
+                description: 'Quis varius quam quisque id diam vel quam elementum pulvinar.',
+                tags: 'Egestas, sed, tempus'
+            },
+            {
+                name: 'Organizacja “Lorem Ipsum 2”',
+                description: 'Hendrerit gravida rutrum quisque non tellus orci ac auctor augue.',
+                tags: 'Ut, aliquam, purus, sit, amet'
+            },
+            {
+                name: 'Organizacja “Lorem Ipsum 3”',
+                description: 'Scelerisque in dictum non consectetur a erat nam.',
+                tags: 'Mi, quis, hendrerit, dolor'
+            },
+            {
+                name: 'Organizacja “Lorem Ipsum 4”',
+                description: 'Scelerisque in dictum non consectetur a erat nam.',
+                tags: 'Mi, quis, hendrerit, dolor'
+            },
+            {
+                name: 'Organizacja “Lorem Ipsum 5”',
+                description: 'Scelerisque in dictum non consectetur a erat nam.',
+                tags: 'Mi, quis, hendrerit, dolor'
+            },
+            {
+                name: 'Organizacja “Lorem Ipsum 6”',
+                description: 'Scelerisque in dictum non consectetur a erat nam.',
+                tags: 'Mi, quis, hendrerit, dolor'
+            },
+            {
+                name: 'Organizacja “Lorem Ipsum 7”',
+                description: 'Scelerisque in dictum non consectetur a erat nam.',
+                tags: 'Mi, quis, hendrerit, dolor'
+            }
+
+
+
+        ]
+
+    },
+    local: {
+        title: 'Lokalnym zbiórkom',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.',
+        items: [
+            {
+                name: 'Zbiórka “Lorem Ipsum 1”',
+                description: 'Quis varius quam quisque id diam vel quam elementum pulvinar.',
+                tags: 'Egestas, sed, tempus'
+            },
+            {
+                name: 'Zbiórka “Lorem Ipsum 2”',
+                description: 'Hendrerit gravida rutrum quisque non tellus orci ac auctor augue.',
+                tags: 'Ut, aliquam, purus, sit, amet'
+            },
+            {
+                name: 'Zbiórka “Lorem Ipsum 3”',
+                description: 'Scelerisque in dictum non consectetur a erat nam.',
+                tags: 'Mi, quis, hendrerit, dolor'
+            },
+            {
+                name: 'Zbiórka “Lorem Ipsum 4”',
+                description: 'Scelerisque in dictum non consectetur a erat nam.',
+                tags: 'Mi, quis, hendrerit, dolor'
+            },
+            {
+                name: 'Zbiórka “Lorem Ipsum 5”',
+                description: 'Scelerisque in dictum non consectetur a erat nam.',
+                tags: 'Mi, quis, hendrerit, dolor'
+            },
+            {
+                name: 'Zbiórka “Lorem Ipsum 6”',
+                description: 'Scelerisque in dictum non consectetur a erat nam.',
+                tags: 'Mi, quis, hendrerit, dolor'
+            },
+            {
+                name: 'Zbiórka “Lorem Ipsum 7”',
+                description: 'Scelerisque in dictum non consectetur a erat nam.',
+                tags: 'Mi, quis, hendrerit, dolor'
+            },
+            {
+                name: 'Zbiórka “Lorem Ipsum 8”',
+                description: 'Scelerisque in dictum non consectetur a erat nam.',
+                tags: 'Mi, quis, hendrerit, dolor'
+            },
+            {
+                name: 'Zbiórka “Lorem Ipsum 9”',
+                description: 'Scelerisque in dictum non consectetur a erat nam.',
+                tags: 'Mi, quis, hendrerit, dolor'
+            }
+
+
+        ]
+
+    }
+};
+
+export default categoriesData;
